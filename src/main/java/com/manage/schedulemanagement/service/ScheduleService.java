e47126151e3b98aa804cbc7b08d2c1d86da9eebd
hyojin <hyojinwjd99@naver.com>
2024-05-20 18:48:45 +0900

    [refactor] password 일치 비교 Object.equals 사용

--- src/main/java/com/manage/schedulemanagement/service/ScheduleService.java
+++ src/main/java/com/manage/schedulemanagement/service/ScheduleService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ScheduleService {
@@ -41,22 +42,26 @@ public class ScheduleService {
 
     @Transactional
     public Long updateSchedule(Long id, ScheduleRequestDto scheduleRequestDto) {
-        Schedule schedule = findScheduleById(id);
-        if (scheduleRequestDto.getPassword().equals(schedule.getPassword())) {
-            schedule.update(scheduleRequestDto);
-        }
+        Schedule schedule = checkPassword(id, scheduleRequestDto.getPassword());
+        schedule.update(scheduleRequestDto);
         return id;
     }
 
     public Long deleteSchedule(Long id, ScheduleRequestDto scheduleRequestDto) {
-        Schedule schedule = findScheduleById(id);
-        if (scheduleRequestDto.getPassword().equals(schedule.getPassword())) {
-            scheduleRepository.delete(schedule);
-        }
+        Schedule schedule = checkPassword(id, scheduleRequestDto.getPassword());
+        scheduleRepository.delete(schedule);
         return id;
     }
 
     private Schedule findScheduleById(Long id) {
         return scheduleRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("선택한 일정은 존재하지 않습니다."));
+    }
+
+    private Schedule checkPassword(Long id, String password) {
+        Schedule schedule = findScheduleById(id);
+        if (!Objects.equals(password, schedule.getPassword()) && schedule.getPassword() != null) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        return schedule;
     }
 }
