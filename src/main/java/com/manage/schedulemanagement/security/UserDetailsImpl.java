0746eb55607c5cf5ea8f210489655cb181578b8a
hyojin <hyojinwjd99@naver.com>
2024-05-30 22:18:50 +0900

    [add] 5단계 데이터베이스에 있는 회원 정보를 Spring Security의 인증 관리자에게 전달 : UserDetailsImpl, UserDetailsServiceImpl

---
+++ src/main/java/com/manage/schedulemanagement/security/UserDetailsImpl.java
@@ -0,0 +1,65 @@
+package com.manage.schedulemanagement.security;
+
+import com.manage.schedulemanagement.entity.UserRoleEnum;
+import com.manage.schedulemanagement.entity.Users;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final Users user;
+
+    public UserDetailsImpl(Users user) {
+        this.user = user;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/manage/schedulemanagement/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.manage.schedulemanagement.security;
+
+import com.manage.schedulemanagement.entity.Users;
+import com.manage.schedulemanagement.repository.UsersRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsersRepository userRepository;
+
+    public UserDetailsServiceImpl(UsersRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
