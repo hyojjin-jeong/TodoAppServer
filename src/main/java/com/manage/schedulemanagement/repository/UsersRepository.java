6bfbe8b11926f2659ee01c68b5f477292a82d37e
hyojin <hyojinwjd99@naver.com>
2024-05-30 22:49:42 +0900

    [add] 5단계 User 데이터베이스 사용을 위한 repository 생성 : UsersRepository

---
+++ src/main/java/com/manage/schedulemanagement/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.manage.schedulemanagement.repository;
+
+import com.manage.schedulemanagement.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+}
