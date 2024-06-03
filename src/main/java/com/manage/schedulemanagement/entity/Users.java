8030596c4e0c03641c4ef54c695490ab04f3faa1
hyojin <hyojinwjd99@naver.com>
2024-06-03 20:18:07 +0900

    [del] setter 어노테이션 삭제

--- src/main/java/com/manage/schedulemanagement/entity/Users.java
+++ src/main/java/com/manage/schedulemanagement/entity/Users.java
@@ -1,17 +1,14 @@
 package com.manage.schedulemanagement.entity;
 
-import com.manage.schedulemanagement.dto.UsersRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "users")
 @NoArgsConstructor
 public class Users extends Timestamped{
